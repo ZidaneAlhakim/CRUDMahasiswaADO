20098634cc0fe0c918e03add9587c1acf884a93e
ZidaneAlhakim <zidanealhakim179@gmail.com>
2026-04-09 15:43:04 +0700

    masukin code nya

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
@@ -37,7 +26,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.txtAlamat = new System.Windows.Forms.TextBox();
-            this.txtKodeProdi = new System.Windows.Forms.TextBox();
+            this.txtkodeProdi = new System.Windows.Forms.TextBox();
             this.cmbJK = new System.Windows.Forms.ComboBox();
             this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
             this.btnConnect = new System.Windows.Forms.Button();
@@ -57,7 +46,6 @@
             this.label1.Size = new System.Drawing.Size(31, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "NIM";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -67,7 +55,6 @@
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nama";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtNIM
             // 
@@ -91,7 +78,6 @@
             this.label4.Size = new System.Drawing.Size(90, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "Jenis Kelamin";
-            this.label4.Click += new System.EventHandler(this.label3_Click);
             // 
             // label5
             // 
@@ -101,7 +87,6 @@
             this.label5.Size = new System.Drawing.Size(90, 16);
             this.label5.TabIndex = 5;
             this.label5.Text = "Tanggal Lahir";
-            this.label5.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
@@ -111,7 +96,6 @@
             this.label3.Size = new System.Drawing.Size(49, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Alamat";
-            this.label3.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
@@ -121,7 +105,6 @@
             this.label6.Size = new System.Drawing.Size(74, 16);
             this.label6.TabIndex = 7;
             this.label6.Text = "Kode Prodi";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // txtAlamat
             // 
@@ -129,14 +112,13 @@
             this.txtAlamat.Name = "txtAlamat";
             this.txtAlamat.Size = new System.Drawing.Size(276, 22);
             this.txtAlamat.TabIndex = 8;
-            this.txtAlamat.TabIndexChanged += new System.EventHandler(this.textBox3_TabIndexChanged);
             // 
-            // txtKodeProdi
+            // txtkodeProdi
             // 
-            this.txtKodeProdi.Location = new System.Drawing.Point(190, 278);
-            this.txtKodeProdi.Name = "txtKodeProdi";
-            this.txtKodeProdi.Size = new System.Drawing.Size(100, 22);
-            this.txtKodeProdi.TabIndex = 9;
+            this.txtkodeProdi.Location = new System.Drawing.Point(190, 278);
+            this.txtkodeProdi.Name = "txtkodeProdi";
+            this.txtkodeProdi.Size = new System.Drawing.Size(100, 22);
+            this.txtkodeProdi.TabIndex = 9;
             // 
             // cmbJK
             // 
@@ -145,7 +127,6 @@
             this.cmbJK.Name = "cmbJK";
             this.cmbJK.Size = new System.Drawing.Size(121, 24);
             this.cmbJK.TabIndex = 10;
-            this.cmbJK.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // dtpTanggalLahir
             // 
@@ -153,7 +134,6 @@
             this.dtpTanggalLahir.Name = "dtpTanggalLahir";
             this.dtpTanggalLahir.Size = new System.Drawing.Size(200, 22);
             this.dtpTanggalLahir.TabIndex = 11;
-            this.dtpTanggalLahir.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // btnConnect
             // 
@@ -163,7 +143,7 @@
             this.btnConnect.TabIndex = 12;
             this.btnConnect.Text = "Membuka Koneksi";
             this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
             // btnLoad
             // 
@@ -173,6 +153,7 @@
             this.btnLoad.TabIndex = 13;
             this.btnLoad.Text = "Menampilkan Data";
             this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // button3
             // 
@@ -182,6 +163,7 @@
             this.button3.TabIndex = 14;
             this.button3.Text = "Menambah Data";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnInsert_Click);
             // 
             // button4
             // 
@@ -191,7 +173,7 @@
             this.button4.TabIndex = 15;
             this.button4.Text = "Mengubah Data";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // button5
             // 
@@ -201,6 +183,7 @@
             this.button5.TabIndex = 16;
             this.button5.Text = "Menghapus Data";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // dataGridView1
             // 
@@ -211,6 +194,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(664, 169);
             this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // Form1
             // 
@@ -225,7 +209,7 @@
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.dtpTanggalLahir);
             this.Controls.Add(this.cmbJK);
-            this.Controls.Add(this.txtKodeProdi);
+            this.Controls.Add(this.txtkodeProdi);
             this.Controls.Add(this.txtAlamat);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
@@ -236,7 +220,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form Data Mahasiswa";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -254,7 +239,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtAlamat;
-        private System.Windows.Forms.TextBox txtKodeProdi;
+        private System.Windows.Forms.TextBox txtkodeProdi;
         private System.Windows.Forms.ComboBox cmbJK;
         private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
         private System.Windows.Forms.Button btnConnect;
@@ -265,4 +250,3 @@
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
-
--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,75 +1,197 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly string connectionString = "Data Source=LAPTOP-M60LBIQK\\ZIDANEAS; Initial Catalog=DBAkademikADO; Integrated Security=True";
+        private readonly SqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
 
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void ConnectDatabase()
         {
-
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void ClearForm()
         {
-
+            txtNIM.Clear();
+            txtNama.Clear();
+            cmbJK.SelectedIndex = -1;
+            txtAlamat.Clear();
+            txtkodeProdi.Clear();
+            dtpTanggalLahir.Value = DateTime.Now;
+            txtNIM.Focus();
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        private void btnConnect_Click(object sender, EventArgs e)
         {
-
+            ConnectDatabase();
         }
 
-        private void label6_Click(object sender, EventArgs e)
+        private void btnLoad_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
 
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("NIM", "NIM");
+                dataGridView1.Columns.Add("Nama", "Nama");
+                dataGridView1.Columns.Add("JenisKelamin", "Jenis Kelamin");
+                dataGridView1.Columns.Add("TanggalLahir", "Tanggal Lahir");
+                dataGridView1.Columns.Add("Alamat", "Alamat");
+                dataGridView1.Columns.Add("KodeProdi", "Kode Prodi");
+
+                string query = "SELECT * FROM Mahasiswa";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["NIM"].ToString(),
+                        reader["Nama"].ToString(),
+                        reader["JenisKelamin"].ToString(),
+                        Convert.ToDateTime(reader["TanggalLahir"]).ToShortDateString(),
+                        reader["Alamat"].ToString(),
+                        reader["KodeProdi"].ToString()
+                    );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void btnInsert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                if (txtNIM.Text == "" || txtNama.Text == "")
+                {
+                    MessageBox.Show("NIM dan Nama harus diisi!");
+                    return;
+                }
 
+                string query = @"INSERT INTO Mahasiswa (NIM, Nama, JenisKelamin, TanggalLahir, Alamat, KodeProdi) 
+                                VALUES (@NIM, @Nama, @JK, @TanggalLahir, @Alamat, @KodeProdi)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
+                cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("@JK", cmbJK.Text);
+                cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
+                cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
+                cmd.Parameters.AddWithValue("@KodeProdi", txtkodeProdi.Text);
+
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("Data berhasil ditambahkan");
+                    ClearForm();
+                    btnLoad_Click(null, null);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Error: " + ex.Message); }
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        // Ini fungsi UPDATE yang tadinya merah
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                string query = @"UPDATE Mahasiswa SET Nama=@Nama, JenisKelamin=@JK, 
+                                TanggalLahir=@TanggalLahir, Alamat=@Alamat, KodeProdi=@KodeProdi WHERE NIM=@NIM";
 
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
+                cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("@JK", cmbJK.Text);
+                cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
+                cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
+                cmd.Parameters.AddWithValue("@KodeProdi", txtkodeProdi.Text);
+
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("Data berhasil diupdate");
+                    btnLoad_Click(null, null);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Error: " + ex.Message); }
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        // Ini fungsi DELETE yang tadinya merah
+        private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (MessageBox.Show("Yakin ingin menghapus data?", "Konfirmasi", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    string query = "DELETE FROM Mahasiswa WHERE NIM=@NIM";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
 
+                    int result = cmd.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Data berhasil dihapus");
+                        ClearForm();
+                        btnLoad_Click(null, null);
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show("Error: " + ex.Message); }
+            }
         }
 
-        private void textBox3_TabIndexChanged(object sender, EventArgs e)
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtNIM.Text = row.Cells[0].Value.ToString();
+                txtNama.Text = row.Cells[1].Value.ToString();
+                cmbJK.Text = row.Cells[2].Value.ToString();
+                dtpTanggalLahir.Value = Convert.ToDateTime(row.Cells[3].Value);
+                txtAlamat.Text = row.Cells[4].Value.ToString();
+                txtkodeProdi.Text = row.Cells[5].Value.ToString();
+            }
         }
     }
 }
